3a7a46b264993166df73d99cbb32575707f1d950
Kishiannn_ <abudachan122@gmail.com>
2025-02-20 21:58:13 +0800

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark" style="margin-bottom: 80px;">
+        <div class="container">
+        <!--Brand -->
+        <a href="" class="navbar-brand">
+            <h4 class="mb-o text-uppercase">SIGBIN 11</h4>
+        </a>
+        <!-- LINKS -->
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="products.php" class="nav-link">Products</a></li>
+            <li class="nav-item"><a href="sections.php" class="nav-link">Sections</a></li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a href="profile.php" class="nav-link">
+                <?= $_SESSION['username']  ?>
+            </a></li>
+            <li class="nav-item"><a href="" class="nav-link">Log out</a></li>
+        </ul>
+        </div>
+    </nav>

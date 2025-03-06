6e52d36a699c78c9bed989cdea2fabc69798f864
Kishiannn_ <abudachan122@gmail.com>
2025-03-06 23:40:45 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -10,10 +10,10 @@
             <li class="nav-item"><a href="sections.php" class="nav-link">Sections</a></li>
         </ul>
         <ul class="navbar-nav ms-auto">
-            <li class="nav-item"><a href="profile.php" class="nav-link">
-                <?= $_SESSION['username']  ?>
+            <li class="nav-item"><a href="profile.php" class="nav-link" style="color: red;">
+                <?=$_SESSION['username']  ?>
             </a></li>
-            <li class="nav-item"><a href="" class="nav-link">Log out</a></li>
+            <li class="nav-item"><a href="logout.php" class="nav-link">Log out</a></li>
         </ul>
         </div>
     </nav>
